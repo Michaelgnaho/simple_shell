400b84bb2f38492a60d219da253aa0a997182f29
Dasurf <damilareyusuf.dy@gmail.com>
2023-12-04 10:57:11 +0100

    little adjustemnts in the header file

--- shell.h
+++ shell.h
@@ -2,10 +2,12 @@
 #define SHELL_H
 #include <stdio.h>
 #include <stdlib.h>
-#include <sting.h>
+#include <string.h>
 #include <unistd.h>
+
 void printf_md(const char *stringmd);
 void dis_prompt(void);
 void execute_cmd(const char *cmd);
 void read_cmd(char *cmd, size_t size);
-#endif
+
+#endif  /* SHELL_H */
