a184b7125c93f491d85170f90b62b8012b10e7a8
Dasurf <damilareyusuf.dy@gmail.com>
2023-12-04 11:02:11 +0100

    had to resolve a conflict on the header file

---
+++ shell.h
@@ -0,0 +1,19 @@
+#ifndef SHELL_H
+#define SHELL_H
+/*
+ * these are the libraries needed
+ */
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+/*
+ * these are the funcion prototypes
+ */
+
+void printf_md(const char *stringmd);
+void dis_prompt(void);
+void execute_cmd(const char *cmd);
+void read_cmd(char *cmd, size_t size);
+
+#endif  /* SHELL_H */
