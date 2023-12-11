4fbc57dc1362cbf510ee318e7a661c1264a78ff0
Michael Gnaho <gnahomichael@gmail.com>
2023-12-11 10:23:46 +0100

    added some builtins

--- execute_cmd.c
+++ execute_cmd.c
@@ -71,24 +71,53 @@ void execute_cmd(const char *cmd)
 	}
 
 */
+
 #include "shell.h"
-#include <sys/wait.h>
+#include "sys/wait.h"
 
 void execute_cmd(const char *cmd) {
-    pid_t child_pid = fork();
+    pid_t child_pid;
+	char *argue[128];
+    child_pid = fork();
+	int argue_count = 0;
 
     if (child_pid == -1) {
+        /* Error forking process */
         write(2, "Error forking process.\n", strlen("Error forking process.\n"));
         exit(EXIT_FAILURE);
     } else if (child_pid == 0) {
-        /* Child process*/
-        if (execlp("/bin/sh", "/bin/sh", "-c", cmd, NULL) == -1) {
-            /* If execlp fails*/
-            write(2, "Error executing command.\n", strlen("Error executing command.\n"));
+        /* Child process */
+
+        /* Create a buffer to store the command */
+        char buffer[150];
+        strcpy(buffer, cmd);
+
+        char *token = strtok(buffer, " ");
+        while (token != NULL && argue_count < 128) {
+            argue[argue_count++] = token;
+            token = strtok(NULL, " ");
+        }
+
+        argue[argue_count] = NULL;
+
+        /* Execute specific commands */
+        if (strcmp(argue[0], "ls") == 0) {
+            execve("/bin/ls", argue, NULL);
+        } else if (strcmp(argue[0], "cat") == 0) {
+            execve("/bin/cat", argue, NULL);
+        } else if (strcmp(argue[0], "pwd") == 0) {
+            execve("/bin/pwd", argue, NULL);
+        } else if (strcmp(argue[0], "clear") == 0) {
+            execve("/usr/bin/clear", argue, NULL);
+        } else if (strcmp(argue[0], "vi") == 0) {
+            execve("/usr/bin/vi", argue, NULL);
+        } else {
+            /* Command not found or not executable */
+            write(2, "Command not found or not executable.\n", strlen("Command not found or not executable.\n"));
             exit(EXIT_FAILURE);
         }
     } else {
-        /* Parent process*/
+        /* Parent process */
         wait(NULL);
     }
 }
