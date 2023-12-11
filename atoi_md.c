e1ab3eef3e2ba8a80f0335588f9c364e4b3a22a6
Michael Gnaho <gnahomichael@gmail.com>
2023-12-11 10:28:17 +0100

    added builtin functions

--- atoi_md.c
+++ atoi_md.c
@@ -1,2 +1,66 @@
 #include "shell.h"
 
+/**
+ * md_is_interactive - returns true if the shell is in interactive mode
+ * @md_info: struct address
+ *
+ * Return: 1 if interactive mode, 0 otherwise
+ */
+int md_is_interactive(md_info_t *md_info)
+{
+    return (isatty(STDIN_FILENO) && md_info->md_readfd <= 2);
+}
+
+/**
+ * md_is_delimiter - checks if character is a delimiter
+ * @c: the char to check
+ * @delim: the delimiter string
+ * Return: 1 if true, 0 if false
+ */
+int md_is_delimiter(char c, char *delim)
+{
+    while (*delim)
+        if (*delim++ == c)
+            return (1);
+    return (0);
+}
+
+/**
+ * md_is_alpha - checks for alphabetic character
+ * @c: The character to input
+ * Return: 1 if c is alphabetic, 0 otherwise
+ */
+int md_is_alpha(int c)
+{
+    return ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'));
+}
+
+/**
+ * md_atoi - converts a string to an integer
+ * @s: the string to be converted
+ * Return: 0 if no numbers in string, converted number otherwise
+ */
+int md_atoi(char *s)
+{
+    int i, md_sign = 1, md_flag = 0, md_output;
+    unsigned int md_result = 0;
+
+    for (i = 0; s[i] != '\0' && md_flag != 2; i++)
+    {
+        if (s[i] == '-')
+            md_sign *= -1;
+
+        if (s[i] >= '0' && s[i] <= '9')
+        {
+            md_flag = 1;
+            md_result *= 10;
+            md_result += (s[i] - '0');
+        }
+        else if (md_flag == 1)
+            md_flag = 2;
+    }
+
+    md_output = (md_sign == -1) ? -md_result : md_result;
+
+    return md_output;
+}
--- md_builtin_1.c
+++ md_builtin_1.c
@@ -1,3 +1,113 @@
 #include "shell.h"
 
 
+/**
+ * md_display_history - Displays the command history, showing each command
+ *                      with its corresponding line number.
+ * @md_info: Structure containing potential arguments.
+ *
+ * Return: Always 0
+ */
+int md_display_history(md_info_t *md_info)
+{
+    print_list(md_info->history);
+    return 0;
+}
+
+/**
+ * md_unset_alias - Removes an alias from the alias list.
+ * @md_info: Parameter struct.
+ * @str: The string representing the alias.
+ *
+ * Return: 0 on success, 1 on error
+ */
+int md_unset_alias(md_info_t *md_info, char *str)
+{
+    char *p;
+    int ret;
+
+    p = _strchr(str, '=');
+    if (!p)
+        return 1;
+    ret = delete_node_at_index(&(md_info->alias),
+                               get_node_index(md_info->alias, node_starts_with(md_info->alias, str, -1)));
+    return ret;
+}
+
+/**
+ * md_set_alias - Sets an alias in the alias list.
+ * @md_info: Parameter struct.
+ * @str: The string representing the alias.
+ *
+ * Return: 0 on success, 1 on error
+ */
+int md_set_alias(md_info_t *md_info, char *str)
+{
+    char *p;
+
+    p = _strchr(str, '=');
+    if (!p)
+        return 1;
+    if (!*++p)
+        return md_unset_alias(md_info, str);
+
+    md_unset_alias(md_info, str);
+    return (add_node_end(&(md_info->alias), str, 0) == NULL);
+}
+
+/**
+ * md_print_alias - Prints an alias string.
+ * @node: The alias node.
+ *
+ * Return: 0 on success, 1 on error
+ */
+int md_print_alias(list_t *node)
+{
+    char *p = NULL, *a = NULL;
+
+    if (node)
+    {
+        p = _strchr(node->str, '=');
+        for (a = node->str; a <= p; a++)
+            _putchar(*a);
+        _putchar('\'');
+        _puts(p + 1);
+        _puts("'\n");
+        return 0;
+    }
+    return 1;
+}
+
+/**
+ * md_alias - Mimics the behavior of the alias builtin in a shell.
+ * @md_info: Structure containing potential arguments.
+ *
+ * Return: Always 0
+ */
+int md_alias(md_info_t *md_info)
+{
+    int i = 0;
+    char *p = NULL;
+    list_t *node = NULL;
+
+    if (md_info->argc == 1)
+    {
+        node = md_info->alias;
+        while (node)
+        {
+            md_print_alias(node);
+            node = node->next;
+        }
+        return 0;
+    }
+    for (i = 1; md_info->argv[i]; i++)
+    {
+        p = _strchr(md_info->argv[i], '=');
+        if (p)
+            md_set_alias(md_info, md_info->argv[i]);
+        else
+            md_print_alias(node_starts_with(md_info->alias, md_info->argv[i], '='));
+    }
+
+    return 0;
+}
